8b23e5b21f4f08953272e6bb22f99a03c2e9cecf
David <parrydavid@live.co.uk>
2022-03-04 12:47:25 +0000

    add eventListener for click when targetting a grid.

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,35 +35,4 @@ document.addEventListener('click', event => {
         target.classList.add(game.xTurn ? 'x' : 'o')
 
         game.xTurn = !game.xTurn
-        
-        // If all cells are disabled, then its draw
-        if (!document.querySelectorAll('.grid:not(.disabled)').length) {
-            document.querySelector('.game-over').classList.add('visible')
-            document.querySelector('.go-text').textContent = 'Draw!'
-        }
 
-        game.winningStates.forEach(winningState => {
-            const xWins = winningState.every(state => game.xState.includes(state))
-            const oWins = winningState.every(state => game.oState.includes(state))
-
-            if (xWins || oWins) {
-                document.querySelectorAll('.grid').forEach(cell => cell.classList.add('disabled'))
-                document.querySelector('.game-over').classList.add('visible')
-                document.querySelector('.go-text').textContent = xWins
-                    ? 'X wins!'
-                    : 'O wins!'
-            }
-        })
-    }
-})
-
-document.querySelector('.restart').addEventListener('click', () => {
-    document.querySelector('.game-over').classList.remove('visible')
-    document.querySelectorAll('.grid').forEach(cell => {
-        cell.classList.remove('disabled', 'x', 'o')
-    })
-
-    game.xTurn = true
-    game.xState = []
-    game.oState = []
-})
